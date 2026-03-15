255bdd63575e828484e629c6ba354707b2868255
Chunyue Ma <cma01@wesleyan.edu>
2026-03-15 16:37:05 +0000

    feat: add model and crud apis for experiences #8

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
-from .views import MyProfileView
+from .views import ExperienceViewSet, MyProfileView
+
+router = DefaultRouter()
+router.register(r"experiences", ExperienceViewSet, basename="experience")
 
 urlpatterns = [
     path("me/", MyProfileView.as_view(), name="my-profile"),
+    path("", include(router.urls)),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,7 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, viewsets
 
-from .serializers import ProfileSerializer
+from .models import Experience
+from .serializers import ExperienceSerializer, ProfileSerializer
 
 
 class MyProfileView(generics.RetrieveUpdateAPIView):
@@ -10,3 +11,16 @@ class MyProfileView(generics.RetrieveUpdateAPIView):
     def get_object(self):
         # This looks up the profile belonging to the logged-in user
         return self.request.user.profile
+
+
+class ExperienceViewSet(viewsets.ModelViewSet):
+    serializer_class = ExperienceSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        # Only return experiences belonging to the logged-in user
+        return Experience.objects.filter(profile__user=self.request.user)
+
+    def perform_create(self, serializer):
+        # Automatically link the experience to the user's profile
+        serializer.save(profile=self.request.user.profile)
